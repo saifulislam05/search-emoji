12f85b063c07045a1bfe10779ab3d7cb1acce09c
saifulislam05 <saiful.bijulia@gmail.com>
2023-11-10 16:28:37 +0530

    project done v1

--- script.js
+++ script.js
@@ -125,26 +125,35 @@ const list = [
   }
 ]
 
-const input_box = document.getElementById("input_box");
-const table_body = document.getElementById("table_body");
 
-document.addEventListener('DOMContentLoaded', () => {
-	renderUi(list);
-})
+const inputBox = document.getElementById("input_box");
+const tableBody = document.getElementById("table_body");
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderUi(list);
+});
 
 function renderUi(list) {
-  table_body.innerHTML = "";
-  list.forEach(emoji => {
-    let aliases=emoji.aliases.join(" ")
-    table_body.innerHTML += `<tr class="capitalize">
-              <td>${emoji.emoji}</td>
-              <td>${aliases}</td>
-              <td>${emoji.description}</td>
-            </tr>`; 
-  })
+  tableBody.innerHTML = "";
+  const fragment = document.createDocumentFragment();
+
+  list.forEach((emoji) => {
+    let aliases = emoji.aliases.join(" ");
+    let description =
+      emoji.description.charAt(0).toUpperCase() + emoji.description.slice(1);
+    let row = document.createElement("tr");
+    row.innerHTML = `
+              <td class="!text-2xl">${emoji.emoji}</td>
+              <td class="capitalize text-base">${aliases}</td>
+              <td class="text-base">${description}</td>
+            `;
+    fragment.appendChild(row);
+  });
+
+  tableBody.appendChild(fragment); 
 }
 
-input_box.addEventListener("input", filterByInput);
+inputBox.addEventListener("input", filterByInput);
 
 function filterByInput(e) {
   const inputValue = e.target.value.toLowerCase();
@@ -156,7 +165,7 @@ function filterList(list, inputValue) {
   return list.filter((emoji) => {
     const searchTerms = [
       emoji.aliases.join(" ").toLowerCase(),
-      emoji.description.toLowerCase()
+      emoji.description.toLowerCase(),
     ];
     return searchTerms.some((term) => term.includes(inputValue));
   });
