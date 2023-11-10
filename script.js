a3ac2671e0f9813e51880d2151cf767afb29a5ab
saifulislam05 <saiful.bijulia@gmail.com>
2023-11-10 16:42:08 +0530

    Emojies Data updated

--- script.js
+++ script.js
@@ -1,136 +1,9 @@
-const list = [
-    {
-      "emoji": "😀"
-    , "description": "grinning face"
-    , "category": "Smileys & Emotion"
-    , "aliases": [
-        "grinning"
-      ]
-    , "tags": [
-        "smile"
-      , "happy"
-      ]
-    , "unicode_version": "6.1"
-    , "ios_version": "6.0"
-    }
-  , {
-      "emoji": "😃"
-    , "description": "grinning face with big eyes"
-    , "category": "Smileys & Emotion"
-    , "aliases": [
-        "smiley"
-      ]
-    , "tags": [
-        "happy"
-      , "joy"
-      , "haha"
-      ]
-    , "unicode_version": "6.0"
-    , "ios_version": "6.0"
-    }
-  , {
-      "emoji": "😄"
-    , "description": "grinning face with smiling eyes"
-    , "category": "Smileys & Emotion"
-    , "aliases": [
-        "smile"
-      ]
-    , "tags": [
-        "happy"
-      , "joy"
-      , "laugh"
-      , "pleased"
-      ]
-    , "unicode_version": "6.0"
-    , "ios_version": "6.0"
-    }
-  , {
-      "emoji": "😁"
-    , "description": "beaming face with smiling eyes"
-    , "category": "Smileys & Emotion"
-    , "aliases": [
-        "grin"
-      ]
-    , "tags": [
-      ]
-    , "unicode_version": "6.0"
-    , "ios_version": "6.0"
-    }
-  , {
-      "emoji": "😆"
-    , "description": "grinning squinting face"
-    , "category": "Smileys & Emotion"
-    , "aliases": [
-        "laughing"
-      , "satisfied"
-      ]
-    , "tags": [
-        "happy"
-      , "haha"
-      ]
-    , "unicode_version": "6.0"
-    , "ios_version": "6.0"
-    }
-  , {
-      "emoji": "😅"
-    , "description": "grinning face with sweat"
-    , "category": "Smileys & Emotion"
-    , "aliases": [
-        "sweat_smile"
-      ]
-    , "tags": [
-        "hot"
-      ]
-    , "unicode_version": "6.0"
-    , "ios_version": "6.0"
-    }
-  , {
-      "emoji": "🤣"
-    , "description": "rolling on the floor laughing"
-    , "category": "Smileys & Emotion"
-    , "aliases": [
-        "rofl"
-      ]
-    , "tags": [
-        "lol"
-      , "laughing"
-      ]
-    , "unicode_version": "9.0"
-    , "ios_version": "10.2"
-    }
-  , {
-      "emoji": "😂"
-    , "description": "face with tears of joy"
-    , "category": "Smileys & Emotion"
-    , "aliases": [
-        "joy"
-      ]
-    , "tags": [
-        "tears"
-      ]
-    , "unicode_version": "6.0"
-    , "ios_version": "6.0"
-    }
-  , {
-      "emoji": "🙂"
-    , "description": "slightly smiling face"
-    , "category": "Smileys & Emotion"
-    , "aliases": [
-        "slightly_smiling_face"
-      ]
-    , "tags": [
-      ]
-    , "unicode_version": "7.0"
-    , "ios_version": "9.1"
-  }
-]
-
 
 const inputBox = document.getElementById("input_box");
 const tableBody = document.getElementById("table_body");
 
 document.addEventListener("DOMContentLoaded", () => {
-  renderUi(list);
+  renderUi(emojiList);
 });
 
 function renderUi(list) {
@@ -157,7 +30,7 @@ inputBox.addEventListener("input", filterByInput);
 
 function filterByInput(e) {
   const inputValue = e.target.value.toLowerCase();
-  const filteredItems = filterList(list, inputValue);
+  const filteredItems = filterList(emojiList, inputValue);
   renderUi(filteredItems);
 }
 
